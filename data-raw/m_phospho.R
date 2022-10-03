e92595583bc9c29939d17bd82ff0a0d7b88710fc
TylerSagendorf <tjsagendorf@gmail.com>
2022-10-03 12:35:55 -0700

    Update m_phospho
    Add gene_symbol, entrez_id, human_uniprot, and human_site columns to fData.

--- data-raw/m_phospho.R
+++ data-raw/m_phospho.R
@@ -11,6 +11,21 @@
 #          site = sub(".*_", "", feature)) %>%
 #   {rownames(.) <- pull(., feature); select(., -feature)}
 #
+# feature_data <- read.delim("../../../master_feature_to_gene_small.txt") %>% filter(feature %in% fData(m_phospho)[["refseq"]])
+#
+# feature_data <- filter(feature_data, entrez_id != "100910990")
+#
+# tmp <- merge(fData(m_phospho), feature_data, by = c("refseq"), sort = FALSE, all.x = T, all.y = FALSE)
+# tmp2 <- merge(tmp, human_sites, by = c("refseq", "site"),
+#               sort = FALSE, all.x = T, all.y = FALSE)
+# tmp2 <- tmp2[, c(1:4, 6, 5)]
+# rownames(tmp2) <- paste0(tmp2$refseq, "_", tmp2$site)
+#
+# fData(m_phospho) <- tmp2[featureNames(m_phospho), ]
+#
+# save(m_phospho, file = "../m_phospho.RData", compress = TRUE)
+#
+#
 # pData(m_phospho) <- pData(m_phospho) %>%
 #   dplyr::rename(timepoint = Training, exp_group = sex_training) %>%
 #   mutate(across(c(pid, vialLabel, bid, plex), as.character),
